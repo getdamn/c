52dead6913ce40e70fdeb6d595d11595256210dd
getdamn <n1mb0606@gmail.com>
2018-08-18 13:45:50 +0900

    2018-08-18 13:45 Diif_Fct Edited

--- project1/test_code/diff_fct/main.c
+++ project1/test_code/diff_fct/main.c
@@ -14,11 +14,29 @@ void prnt_arr(const int len, const char* const arr)
 {
 	int i = 0;	//Prints array with numbers
 	printf("[");
-	for (i = 0; i<len - 1; i++)
-		printf("%d, ", arr[i]);
-	printf("%d", arr[len - 1]);
+	for (i = 0; i<len; i++)
+	{
+		if(arr[i] == FILL) printf("O");
+		else if(arr[i] == CROSS) printf("X");
+		else printf("_");
+	}
 	printf("]");
 	printf("\n");
+}
+int iea(char* arr, int len, char emp_char)
+{
+	int i;
+	for(i = 0; i < len; i ++)
+		if(arr[i] != emp_char) return FALSE;
+	return TRUE;
+}
+int issub(char* case_arr, char* comp, int len, char emp_char) //chk case_arr is sub
+{
+	int i;
+	for(i = 0; i < len; i++)
+		if((comp[i] !=  emp_char) & (comp[i] != case_arr[i]))
+			return FALSE;
+	return TRUE;
 }
 void dac(char* org, char* res, char* parr, char* nlist, int plen)
 {
@@ -41,43 +59,45 @@ void dac(char* org, char* res, char* parr, char* nlist, int plen)
 		}
 	}
 	
-	if(iea(org, nemo_size)  == TRUE) //short of 'is empty string'
+	if(iea(org, nemo_size, EMPTY)  == TRUE) //short of 'is empty string'
 	{
-		if(iea(res, nemo_size) == TRUE) //if result is empty, fill case in result...
+		if(iea(res, nemo_size, EMPTY) == TRUE) //if result is empty, fill case in result...
 			memcpy(res, arr_case, sizeof(char) * nemo_size);
 		else
 		{
 			for(i = 0; i < nemo_size; i++)
 			{
-				if(org[i] != arr_case[i])
+				if(res[i] != arr_case[i])
 					res[i] = EMPTY;
 			}
 		}
 	}
 	else
 	{
-		if(iea(res, nemo_size) == TRUE)
-		{
-			if(issub(arr_case, org, nemo_size) == TRUE)//issub checks is arr is sub.
+		if(issub(arr_case, org, nemo_size, EMPTY) == TRUE)
+		{//issub checks is arr is sub
+			if(iea(res, nemo_size, EMPTY) == TRUE)			
 				memcpy(res, arr_case, sizeof(char) * nemo_size);
-		}
-		else
-		{
-			for(i = 0; i < nemo_size; i++)
+			else
 			{
-				if(org[i] != arr_case[i])
-					res[i] = EMPTY;
+				for(i = 0; i < nemo_size; i++)
+				{
+					if(res[i] != arr_case[i])
+						res[i] = EMPTY;
+				}
 			}
 		}
 	}
 	//TEST CASE
+	printf("case: ");
+	prnt_arr(nemo_size, arr_case);
 	printf("orgin : ");
 	prnt_arr(nemo_size, res);
 }
 int main(int argc, char *argv[]) {
-	char org[] = {0,0,0,0,0,0,0,0,0,0};
-	char res[] = {0,0,0,0,0,0,0,0,0,0};
-	char parr[] = {1,1,1,1};
+	char org[] = {1,0,0,0,0,0,0,0,0,0};
+	char res[] = {1,-1,1,1,-1,1,1,1,-1,-1};
+	char parr[] = {0,1,2,1};
 	char nlist[] = {1,2,3};
 	
 	dac(org, res, parr, nlist, 4);
